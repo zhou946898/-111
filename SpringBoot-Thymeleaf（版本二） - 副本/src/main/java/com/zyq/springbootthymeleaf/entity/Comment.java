dfcfed59b622b8a43d676be8015f46213a72c8d6
zhou946898 <2229339794@qq.com>
2025-06-12 09:28:51 +0800

    Add files via upload

---
+++ SpringBoot-Thymeleaf（版本二） - 副本/src/main/java/com/zyq/springbootthymeleaf/entity/Comment.java
@@ -0,0 +1,68 @@
+package com.zyq.springbootthymeleaf.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private Date commentDate;
+
+    @ManyToOne
+    private Article article;
+
+    @ManyToOne
+    private User user;
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(Date commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
